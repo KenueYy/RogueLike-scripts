ab549b9422f788fecf34b4a5fd4548a8315267b6
KenueY <kenueyy@yandex.ru>
2022-04-06 02:34:20 +0700

    Fix Chest

--- Objects/Chest.cs
+++ Objects/Chest.cs
@@ -6,33 +6,14 @@ using UnityEngine.Events;
 public class Chest : MonoBehaviour,IOpening
 {
     public int price;
-    public UnityEvent<GameObject> ChestOpened;
     private bool isOpened = false;
     private bool isEmpty = false;
-    private bool playerNearby = false;
     private Animator _anim;
-    private GameObject _chest;
     public void Start()
     {
         _anim = GetComponent<Animator>();
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.GetComponent<PlayerActions>() != null)
-        {
-            _chest = this.gameObject;
-            playerNearby = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.GetComponent<PlayerActions>() != null)
-            this.playerNearby = false;
-    }
-    private void Update()
-    {
-        ChestOpened?.Invoke(_chest);
-    }
+
     public void Open()
     {
         if (!isOpened)
